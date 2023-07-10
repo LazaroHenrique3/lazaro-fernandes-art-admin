3345b3af8cb7513f3f28d83554244e124da5ce52
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-07-10 09:18:23 -0300

    feat: Finalizando a barra de detalhes

--- src/shared/components/DetailTools/index.tsx
+++ src/shared/components/DetailTools/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Icon, Paper, useTheme, Divider } from '@mui/material'
+import { Box, Button, Icon, Paper, useTheme, Divider, Skeleton, Typography, useMediaQuery } from '@mui/material'
 
 interface IDetailTools {
     newButtonText?: string
@@ -8,6 +8,11 @@ interface IDetailTools {
     showDeleteButton?: boolean
     showSaveButton?: boolean
 
+    showNewButtonLoading?: boolean
+    showBackButtonLoading?: boolean
+    showDeleteButtonLoading?: boolean
+    showSaveButtonLoading?: boolean
+
     onClickNewButton?: () => void
     onClickBackButton?: () => void
     onClickDeleteButton?: () => void
@@ -15,12 +20,17 @@ interface IDetailTools {
 }
 
 export const DetailTools: React.FC<IDetailTools> = ({
-    newButtonText =  'Novo',
+    newButtonText = 'Novo',
 
     showNewButton = true,
     showBackButton = true,
     showDeleteButton = true,
     showSaveButton = true,
+
+    showNewButtonLoading = false,
+    showBackButtonLoading = false,
+    showDeleteButtonLoading = false,
+    showSaveButtonLoading = false,
 
     onClickNewButton,
     onClickBackButton,
@@ -29,22 +39,66 @@ export const DetailTools: React.FC<IDetailTools> = ({
 }) => {
     const theme = useTheme()
 
+    //Retorna true ou false de acordo com o tamanho da tela
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'))
+    const mdDown = useMediaQuery(theme.breakpoints.down('md'))
+
     return (
         <Box marginX={1} padding={1} paddingX={2} display='flex' alignItems='center' gap={1} height={theme.spacing(5)} component={Paper}>
-            {showSaveButton && (
-                <Button color='primary' variant='contained' startIcon={<Icon>save</Icon>} onClick={onClickSaveButton}>Salvar</Button>
-            )}
-            {showDeleteButton && (
-                <Button color='error' variant='outlined' startIcon={<Icon>delete</Icon>} onClick={onClickDeleteButton}>Apagar</Button>
-            )}
-            {showNewButton && (
-                <Button color='success' variant='outlined' startIcon={<Icon>add</Icon>} onClick={onClickNewButton}>Novo</Button>
+            {(showSaveButton && !showSaveButtonLoading) && (
+                <Button color='primary' variant='contained' startIcon={<Icon>save</Icon>} onClick={onClickSaveButton}>
+                    <Typography variant='button' fontSize={12} fontWeight={600} noWrap>
+                        Salvar
+                    </Typography>
+                </Button>
             )}
 
-            <Divider variant='middle' orientation='vertical'/>
+            {showSaveButtonLoading && (
+                <Skeleton width={110} height={60} />
+            )}
 
-            {showBackButton && (
-                <Button color='primary' variant='outlined' startIcon={<Icon>arrow_back</Icon>} onClick={onClickBackButton}>Voltar</Button>
+            {(showNewButton && !showNewButtonLoading && !smDown) && (
+                <Button color='success' variant='outlined' startIcon={<Icon>add</Icon>} onClick={onClickNewButton}>
+                    <Typography variant='button' fontSize={12} fontWeight={600} noWrap>
+                        {newButtonText}
+                    </Typography>
+                </Button>
+            )}
+
+            {(showNewButtonLoading && !smDown) && (
+                <Skeleton width={110} height={60} />
+            )}
+
+            {(showDeleteButton && !showDeleteButtonLoading) && (
+                <Button color='error' variant='outlined' startIcon={<Icon>delete</Icon>} onClick={onClickDeleteButton}>
+                    <Typography variant='button' fontSize={12} fontWeight={600} noWrap>
+                        Apagar
+                    </Typography>
+                </Button>
+            )}
+
+            {showDeleteButtonLoading && (
+                <Skeleton width={110} height={60} />
+            )}
+
+            {
+                (showBackButton &&
+                    (showNewButton || showDeleteButton || showSaveButton)
+                ) && (
+                    < Divider variant='middle' orientation='vertical' />
+                )
+            }
+
+            {(showBackButton && !showBackButtonLoading) && (
+                <Button color='primary' variant='outlined' startIcon={<Icon>arrow_back</Icon>} onClick={onClickBackButton}>
+                    <Typography variant='button' fontSize={12} fontWeight={600} noWrap>
+                        Voltar
+                    </Typography>
+                </Button>
+            )}
+
+            {showBackButtonLoading && (
+                <Skeleton width={110} height={60} />
             )}
         </Box>
     )
