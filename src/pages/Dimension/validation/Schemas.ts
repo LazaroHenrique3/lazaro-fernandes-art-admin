66b5ae1ce4fe4f07bc11ef83dc4ab588058253ee
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-08-19 14:33:08 -0300

    feat: Ajustes no validation de dimension

--- src/pages/Dimension/validation/Schemas.ts
+++ src/pages/Dimension/validation/Schemas.ts
@@ -7,5 +7,5 @@ export interface IFormData {
 //Definindo o schema para validação
 export const formatValidationSchema: yup.Schema<IFormData> = yup.object().shape({
     dimension: yup.string().required().min(3).max(20)
-        .matches(/^\d+ x \d+$/, 'Formato inválido. Use o formato: "20 x 30"')
+        .matches(/^\d+ x \d+ x \d+$/, 'Formato inválido. Use o formato: "20 x 30 x 3"')
 })
