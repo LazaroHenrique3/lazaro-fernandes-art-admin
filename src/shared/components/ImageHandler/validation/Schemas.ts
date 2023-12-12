d2a28f4c52ade2e61c472883c6514819a1a49b6f
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-12-12 20:22:43 -0300

    feat: Utilizando as novas funções de validação de imagem

--- src/shared/components/ImageHandler/validation/Schemas.ts
+++ src/shared/components/ImageHandler/validation/Schemas.ts
@@ -1,5 +1,5 @@
 import * as yup from 'yup'
-import { isValidDimensions, PRODUCT_IMAGE } from '../../../util/validationUtils'
+import { existsImage, isValidMainImageDimensions, isValidSize, isValidType } from '../../../util/validationUtils/product'
 
 interface IFormDataUpdateImage {
     image: FileList
@@ -8,63 +8,48 @@ interface IFormDataUpdateImage {
 //Definindo o schema para validar as imagens na hora de editar
 export const formatValidationSchemaUpdateImage: yup.Schema<IFormDataUpdateImage> = yup.object().shape({
     image: yup.mixed()
-        .test('isImage', (value) => {
+        .test('isImage', async (value) => {
             const mainImage: FileList = value as FileList
 
             // Obtendo a primeira imagem do FileList
             const image = mainImage[0]
 
             //Verificando se foi passado imagem
-            if (mainImage.length === 0) {
-                throw new yup.ValidationError('A imagem é obrigatória!', value, 'image')
-            }
+            existsImage(mainImage.length, value)
 
             //Verificando o formato das imagens
-            const supportedFormats = ['image/jpeg', 'image/png', 'image/jpg']
-            if (!supportedFormats.includes(mainImage[0].type)) {
-                throw new yup.ValidationError('Formato de imagem inválido!', value, 'image')
-            }
+            isValidType(image.type, value)
 
             // Verifica se o tamanho da imagem é maior que 2MB (em bytes)
-            const maxSize = 2 * 1024 * 1024 // 2MB
-            if (Number(mainImage[0].size) > maxSize) {
-                throw new yup.ValidationError('Tamanho de imagem excede 2MB!', value, 'image')
-            }
+            isValidSize(image.size, value)
 
-            //Verificando as dimensões recomendadaas
-            // Criando um URL temporário para a imagem
-            const imageUrl = URL.createObjectURL(image)
+            //Verificando as dimensões recomendadas
+            await isValidMainImageDimensions(image, value)
 
-            // Criando uma promessa para lidar com a carga da imagem
-            return new Promise((resolve, reject) => {
-                const img = new Image()
-                img.src = imageUrl
+            return true
+        })
+        .required() as yup.Schema<FileList>,
+})
 
-                img.onload = () => {
-                    // Dimensões estão corretas
-                    if (isValidDimensions( 
-                        PRODUCT_IMAGE.MAX_W_MAIN_IMAGE, 
-                        PRODUCT_IMAGE.MAX_H_MAIN_IMAGE, 
-                        PRODUCT_IMAGE.MIN_W_MAIN_IMAGE, 
-                        PRODUCT_IMAGE.MIN_H_MAIN_IMAGE, 
-                        img.height, 
-                        img.width)) {
-                        resolve(true)
-                    } else {
-                        // Dimensões não correspondem às recomendadas
-                        reject(
-                            new yup.ValidationError(
-                                'Ás dimensões deve ser entre (1070 a 1090 X 705 a 725)pixels.', value, 'image'
-                            )
-                        )
-                    }
+//Definindo o schema que será utilizado em caso d ealteração de imagem do Cliente
+export const formatValidationSchemaUpdateCustomerImage: yup.Schema<IFormDataUpdateImage> = yup.object().shape({
+    image: yup.mixed()
+        .test('isImage', async (value) => {
+            const mainImage: FileList = value as FileList
 
-                    // Liberar o URL temporário após o uso
-                    URL.revokeObjectURL(imageUrl)
-                }
+            // Obtendo a primeira imagem do FileList
+            const image = mainImage[0]
 
-                return true
-            })
+            //Verificando se foi passado imagem
+            existsImage(mainImage.length, value)
+
+            //Verificando o formato das imagens
+            isValidType(image.type, value)
+
+            // Verifica se o tamanho da imagem é maior que 2MB (em bytes)
+            isValidSize(image.size, value)
+
+            return true
         })
         .required() as yup.Schema<FileList>,
 })
